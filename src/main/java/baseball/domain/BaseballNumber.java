6c0dc7d7305318511ef94888cae43f1298559385
SSung023 <hat7206@naver.com>
2023-11-26 14:58:03 +0900

    feat: 야구 게임 번호를 저장하는 일급컬렉션 생성 및 검증 로직 추가

--- src/main/java/baseball/domain/BaseballNumber.java
+++ src/main/java/baseball/domain/BaseballNumber.java
@@ -1,5 +1,8 @@
 package baseball.domain;
 
+import static baseball.exception.ErrorCode.INVALID_BASEBALL_NUMBER;
+import static baseball.exception.ErrorCode.INVALID_NUMBER_SIZE;
+
 import java.util.Collections;
 import java.util.List;
 
@@ -12,6 +15,35 @@ public class BaseballNumber {
 
     public static BaseballNumber create(List<Integer> baseballNumber) {
         return new BaseballNumber(baseballNumber);
+    }
+
+    private void validate(List<Integer> baseballNumber) {
+        validateSize(baseballNumber);
+        validateRange(baseballNumber);
+        validateDuplicate(baseballNumber);
+    }
+
+    private void validateSize(List<Integer> baseballNumber) {
+        if (baseballNumber.size() != 3) {
+            throw new IllegalArgumentException(INVALID_NUMBER_SIZE.getMessage());
+        }
+    }
+
+    private void validateRange(List<Integer> baseballNumber) {
+        boolean outOfRange = baseballNumber.stream()
+                .anyMatch(num -> num < 1 || 9 < num);
+        if (outOfRange) {
+            throw new IllegalArgumentException(INVALID_BASEBALL_NUMBER.getMessage());
+        }
+    }
+
+    private void validateDuplicate(List<Integer> baseballNumber) {
+        int distinctCount = (int) baseballNumber.stream()
+                .distinct()
+                .count();
+        if (baseballNumber.size() != distinctCount) {
+            throw new IllegalArgumentException(INVALID_BASEBALL_NUMBER.getMessage());
+        }
     }
 
     public List<Integer> getBaseballNumber() {
